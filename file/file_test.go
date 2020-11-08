07b6d544d62d5ec802dbbca44ff44c234a648a1e
Fernandez Ludovic <ludovic@containo.us>
2020-11-08 23:21:47 +0100

    chore: refactor tests.

--- file/file_test.go
+++ file/file_test.go
@@ -145,58 +145,62 @@ yi: {}
 }
 
 func TestDecodeContent_YAML_rawValue(t *testing.T) {
-	content := `
-name: test
-meta:
-  aaa:
-  - bbb: 1
-`
-
 	type Foo struct {
 		Name string
 		Meta map[string]interface{}
 	}
 
-	element := &Foo{}
-
-	err := DecodeContent(content, ".yaml", element)
-	require.NoError(t, err)
-
-	expected := &Foo{
-		Name: "test",
-		Meta: map[string]interface{}{"aaa": []interface{}{map[string]interface{}{"bbb": "1"}}},
-	}
-	assert.Equal(t, expected, element)
-}
-
-func TestDecodeContent_YAML_emptyKey(t *testing.T) {
-	content := `
+	testCases := []struct {
+		desc     string
+		content  string
+		expected interface{}
+	}{
+		{
+			desc: "simple",
+			content: `
+name: test
+meta:
+  aaa:
+  - bbb: 1
+`,
+			expected: &Foo{
+				Name: "test",
+				Meta: map[string]interface{}{"aaa": []interface{}{map[string]interface{}{"bbb": "1"}}},
+			},
+		},
+		{
+			desc: "null",
+			content: `
 name: test
 meta:
   aaa:
   - bbb: 1
   bbb: {"foo":"bar"}
   null: {"toto":"tata"}
-`
-
-	type Foo struct {
-		Name string
-		Meta map[string]interface{}
-	}
-
-	element := &Foo{}
-
-	err := DecodeContent(content, ".yaml", element)
-	require.NoError(t, err)
-
-	expected := &Foo{
-		Name: "test",
-		Meta: map[string]interface{}{
-			"aaa": []interface{}{map[string]interface{}{"bbb": "1"}},
-			"bbb": map[string]interface{}{"foo": "bar"},
+`,
+			expected: &Foo{
+				Name: "test",
+				Meta: map[string]interface{}{
+					"aaa": []interface{}{map[string]interface{}{"bbb": "1"}},
+					"bbb": map[string]interface{}{"foo": "bar"},
+				},
+			},
 		},
 	}
-	assert.Equal(t, expected, element)
+
+	for _, test := range testCases {
+		test := test
+		t.Run(test.desc, func(t *testing.T) {
+			t.Parallel()
+
+			element := &Foo{}
+
+			err := DecodeContent(test.content, ".yaml", element)
+			require.NoError(t, err)
+
+			assert.Equal(t, test.expected, element)
+		})
+	}
 }
 
 func TestDecodeContent_JSON(t *testing.T) {
