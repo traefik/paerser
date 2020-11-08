f575a885b26793d01f027c6caddec7c29e31c7c9
Romain Beuque <556072+rbeuque74@users.noreply.github.com>
2020-11-08 23:16:51 +0100

    fix: empty value for key map should be skipped to prevent panic (#5)

--- file/file_test.go
+++ file/file_test.go
@@ -169,6 +169,36 @@ meta:
 	assert.Equal(t, expected, element)
 }
 
+func TestDecodeContent_YAML_emptyKey(t *testing.T) {
+	content := `
+name: test
+meta:
+  aaa:
+  - bbb: 1
+  bbb: {"foo":"bar"}
+  null: {"toto":"tata"}
+`
+
+	type Foo struct {
+		Name string
+		Meta map[string]interface{}
+	}
+
+	element := &Foo{}
+
+	err := DecodeContent(content, ".yaml", element)
+	require.NoError(t, err)
+
+	expected := &Foo{
+		Name: "test",
+		Meta: map[string]interface{}{
+			"aaa": []interface{}{map[string]interface{}{"bbb": "1"}},
+			"bbb": map[string]interface{}{"foo": "bar"},
+		},
+	}
+	assert.Equal(t, expected, element)
+}
+
 func TestDecodeContent_JSON(t *testing.T) {
 	content := `
 {
